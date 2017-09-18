cdeb228d6c4a07d8bfee2a9581e475c9b981a31b
anndre <anndre@anndre-PC>
2017-09-18 18:53:40 -0300

    lab 01

---
+++ src/lab01.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner; 
+public class lab01 {
+	
+
+	public static void main(String[]args){
+		
+		final int NÚMEROS_A_LER=3;
+	    Scanner sc= new Scanner(System.in);
+	    int minimo= Integer.MAX_VALUE;
+	    int maximo= Integer.MIN_VALUE;
+	    
+	    for(int i= 0; i<NÚMEROS_A_LER; i++) {
+	    	System.out.println("entre com o próximo número inteiro");
+	    	int num = sc.nextInt();
+	    	if(num<minimo) {
+	    		minimo = num;
+	    	}
+	    	if (num>maximo){
+	    		maximo=num;
+	    	
+	    }
+	    	
+	   
+	         
+
+	    }	
+	    System.out.println("o maior número é:"+maximo);
+        System.out.println("o menor número é:"+minimo);
+}
+	
+}
